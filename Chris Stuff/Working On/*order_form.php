fea19679851bfbbe6f6eaa5be2202da3b8963fbf
chrisjtumble1 <64096252+chrisjtumble1@users.noreply.github.com>
2020-11-15 08:13:55 -0500

    Order-Insert
    Customer Insert table worked on, considered which values would go to which DB tables, and which DB tables need to display for example Inventory info or a user name , so which db to get data from and which to give data to.

---
+++ Chris Stuff/Working On/*order_form.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<form>
+		<h2>Notes:</h2>
+		Registered or Not(Default Currently Not Registered):
+		<br/>
+		Registration Requires Sessions
+		<br/>
+		Using Cart Functionality Requires Sessions 
+		<br/>
+		<h2>Hey customer! Ready to place your order?!?</h2>
+				<br/>
+		Registered:[Output Guest ID]
+				<br/>
+		Order Name: [Order Info Obtained From Inventory DB]
+				</br/>
+		Requested Pick-up time: <input type="text" name="phone"></input>
+				</br/>
+
+		Customer Name:
+
+		Phone #: <input type="text" name="user"></input>
+				</br/>
+		Email: <input type="text" name="password"></input>
+				</br/>
+
+		password:
+		
+		verify password: 
+
+		To Be Added - Optional Register - Add User Name
+		Special Note: <input type="text" name="confirm"></input>
+				</br/>
+				<input type="submit" value="Place Order"></input>
+	</form>
+</body>
+</html>
