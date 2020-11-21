1b0ed40f05d11ad2421642637e4bc84dd82b01e0
chrisjtumble <chrisjtumble@users.noreply.github.com>
2020-11-21 10:00:51 -0500

    newest

---
+++ Chris-files/order_form.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<form>
+		<h2>Notes:</h2>
+		Registered or Not(Default Currently Not Registered):
+		<br/>
+		Registration Requires Sessions
+		<br/>
+		Using Cart Functionality Requires Sessions 
+		<br/>
+		<h2>Hey customer! Ready to place your order?!?</h2>
+				<br/>
+		<b>Registered:[Output Guest ID]</b>
+			<?php
+				#This Code automatically gener
+
+
+				#Create Row for new user
+				#Get guest + # of Row
+			?>
+
+
+				<br/>
+		Order Name:
+			<?php
+				#Output All the order information based on the order number that is selected
+			?>
+				</br/>
+		Requested Pick-up time: <input type="text" name="phone"></input>
+				</br/>
+
+		Customer Name: <input type="text" name="name"></form>
+
+		Phone #: <input type="text" name="user"></input>
+				</br/>
+		Email: <input type="text" name="password"></input>
+				</br/>
+
+		New password:
+		
+		verify password: 
+		<?php
+			#If New Password == Verify Password
+			#Insert New Information Into user-table
+		?>
+
+
+		To Be Added - Optional Register - Add User Name
+		Special Note: <input type="text" name="confirm"></input>
+				</br/>
+				<input type="submit" value="Place Order"></input>
+	</form>
+</body>
+</html>
