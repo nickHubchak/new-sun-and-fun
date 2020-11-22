363d4ee8d1b483670db8f600b7d708612f658d5a
chrisjtumble <chrisjtumble@users.noreply.github.com>
2020-11-21 21:21:02 -0500

    Updated view.php, to be close to fully functional.
    All that is missing is a use of the sql edit
    And a use of the sql insert, given the lines of code 94-115

--- Chris-files/view.php
+++ Chris-files/view.php
@@ -1,180 +1,129 @@
-<?php
-
-$con = mysqli_connect('localhost', 'root', '', 'login');
-if(!$con)
-{
-
-	echo 'Not Connected To Server';
-}
-else{
-	//echo 'successful connection to server <br/>';
-}
-
-//Select DataBase: In our case database is named users
-if(!mysqli_select_db($con,'login')){
-	echo 'The database was not selected <br/>';
-}
-else{
-	//echo 'success, everything works.';
-
-	}
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>View</title>
-<link rel="stylesheet" href="css/style.css" />
+	<meta charset="utf-8">
+	<title></title>
 </head>
 <body>
-<p><a href="index.php">Home</a> | <a href="insert.php"><b>Insert</b></a> | <a href="logout.php">Logout</a></p>
-<h2>Inventory DB</h2>
-<table width="100%" border="1" style="border-collapse:collapse;">
-	<thead>
-		<tr>
-			<th>Inventory Number</th>
-			<th>Product Name</th>
-			<th>Quantity Available</th>
-			<th>Attribute Name</th>
-			<th>Attribute Name</th>
-			<th>Edit</th>
-			<th>Delete</th>
-		</tr>
-	</thead>
 	<?php
+		$con = mysqli_connect('localhost', 'root', '', 'sunnfun');
+
+		if(!$con)
+		{
+			echo 'Not Connected To Server';
+		}
+		else{
+			echo 'successful connection to server<br/>';
+		}
+
+	//Select DataBase: In our case database is named users
+	if(!mysqli_select_db($con,'sunnfun')){
+	echo 'The database was not selected';
+	}
+	else{
+		echo 'The database was successfully connected';
+	}
+	?>
 	
-		$count=1;
-		//Can use FROM to alphebatize orders
-		$sql="Select * from Users;";
-		$result = mysqli_query($con,$sql);
-		while($row = mysqli_fetch_assoc($result)){ 
-			?>
-			<tr>
-				<td><?php echo $count; $count++; ?></td>
-				<td>Insert Product Name
-					<?php 
-					#echo $row["name"];     
-
-
-					?>
-				</td>
-				<td>Insert Quantity
-					<?php 
-					
-
-					#echo $row["email"];
-					?>
-				</td>
-				<td>?</td>
-				<td>?</td>
-				<td><a href="edit.php?id=<?php echo $row["id"]; ?>"><b>Edit</b></a></td>
-				<td><a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
-			</tr>
-
-			 
-	<?php  } 
-		//While loop terminates here to produce more columns.
-		?> 
-		</table>
-
-
-<?php
-
-//If No table --> Do Nothing
-//If order Processed -> Contact User
-//If order Fulfilled -> Modify Inventory Table 
-
-//Order Process table --> Has additional customer Info
-// Else --> Get info From user signing up
-?>
-
-<h2>Queue DB</h2>
-<table width="100%" border="1" style="border-collapse:collapse;">
-	<thead>
-		<tr>
-			<th>T-ID</th>
-			<th>List of Order Items w/ quantity(All product details go here)</th>
-			<th>Requested Pick up time</th>
-			<th>Mark as Processed</th>
-			<th>Mark as Complete</th>
-			<th>Time Ordered</th>
-			<th>Customers Phone #</th>
-			<th>Customers Email Address</th>
-			<th>Customer Special Instructions</th>
-		</tr>
-	</thead>
-	<?php
-	
-		$count=1;
-
-		//Can use FROM to alphebatize orders
-		$sql="Select * from Users;";
-		$result = mysqli_query($con,$sql);
-		while($row = mysqli_fetch_assoc($result)){ 
-			?>
-			<tr>
-				<td><?php echo $count; $count++; ?></td>
-				<td>View Order Info x [# of quantity]
-					<?
-
-					#php echo $row["name"];     
-					?>
-						
-				</td>
+	<form>
+		Using Cart Functionality Requires Sessions 
+		<br/>
+		<h2>Customer Clicks Check out and ends up here.</h2>
+		<h2>Sign up in this case to complete the order.</h2>
+		
+				<br/>
+		
+			<?php
 				
-				<td>Insert Pick-Up time
-					<?php 
-						#echo $row["email"];		 
+				$firstname = "catch";
+				$lastname = "";
+				$phone_number = "";
+				$username = "";
+				$password = "";
+				
+				$sql = "INSERT INTO customer(firstname,lastname,phone_number, username, password) VALUES ('$firstname','$lastname','$phone_number','$username','$password')";
+				if(mysqli_query($con,$sql)){
+					echo '<b>Guest ID:</b>';
+				}
+				else{
+					echo 'fail';
+				}
+
+				#Get UserID with the name catch and take guest + UniqueID as the user name.
+
+				$sql = "SELECT * FROM customer WHERE firstname='catch';";
+				$result = mysqli_query($con,$sql);
+				//output data to table
+
+				$resultCheck = mysqli_num_rows($result);
+				$username = "";
+
+				if($row=mysqli_fetch_assoc($result)){
+					$username = "guest".$row['customer_id'];
+				}
+				echo($username);
+				echo('<br/>');
+				
+				
+				
+			?>
 
 
-					?>
-							
-				</td>
-				<td>?</td>
-				<td>?</td>
-				<td>?</td>
-				<td>Insert Phone
+				<br/>
+		Order Name:
+			<?php
+				#Output All the order information based on the order number that is selected
+			?>
+				<br/>
+		Requested Pick-up time: <input type="text" name="pickup"></input>
+				<br/>
+
+		First Name: <input type="text" name="first"></form>
+		Last Name: <input type="text" name="last"></form>
+
+		Phone #: <input type="text" name="phone_number"></input>
+				<br/>
+		Email: <input type="text" name="email"></input>
+				<br/>
+
+		Special Note: <input type="text" name="note"></input>
+				</br/>
+				<input type="submit" value="Sign-Up & Reserve-Order"></input>
+				
+		<?php
+				$guestUser = $username;
+				$firstname = $_POST['first'];
+				$lastname = $_POST['last'];
+				$phone_number = $_POST['phone_number'];
+				$password = "disabled";
+				$current_time = "Current Time";
+				
+				
+				#$update="update new_record set trn_date='".$trn_date."', name='".$name."', age='".$age."', submittedby='".$submittedby."' where id='".$id."'";
+				#mysqli_query($con, $update) or die(mysqli_error());
+				
+				
+				
+				$markProcessed = False;
+				$markCompleted = False;
+				$requested_pickup_time = &_POST['pickup'];
+				$Email = $_POST['email'];
+				SpecialNote = $_POST['note'];
+				
+				$sql = "INSERT INTO order_queue(requested_pickup,mark_processed,mark_complete, pass) VALUES ('$name','$email','$phone','$user','$password')";
 
 
-					<?php     
-						#Insert Phone Number Here
-					?>
+				if(mysqli_query($con,$sql)){
+					echo 'successful';
+				}
+				else{
+					echo 'fail';
+				}
+				
+				
 
-
-				</td>
-				<td>Insert Email
-
-					<?php
-						#Insert Email
-
-					?>
-
-				</td>
-				<td>Insert Special Instruction
-
-					<?php
-
-						#Insert Special Instructions
-
-					?>
-
-
-				</td>
-			</tr>
-
-			 </table>
-	<?php  } 
-		//While loop terminates here to produce more columns.
-		?> 
-
-
-
-
-
-
-
-
-
+			?>
+		
+	</form>
 </body>
 </html>
